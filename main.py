e75edf77819f64736c48e19dcfec76e372c1f819
German Cordova <german.sc937@gmail.com>
2023-08-23 19:56:31 -0500

    Initial Commit

---
+++ main.py
@@ -0,0 +1,84 @@
+from flask import Flask, request, jsonify
+from flask_cors import CORS
+import pickle
+import numpy as np
+import nltk
+from nltk.stem import WordNetLemmatizer
+from sklearn.feature_extraction.text import CountVectorizer
+from keras.models import load_model
+import json
+import random
+
+intents = json.loads(open('intents.json', 'r', encoding='utf-8').read())
+
+# Load the vectorizer vocabulary
+vectorizer_vocab = pickle.load(open('vectorizer_vocab.pkl', 'rb'))
+vectorizer = CountVectorizer(vocabulary=vectorizer_vocab)
+
+app = Flask(__name__)
+app.config['JSON_AS_ASCII'] = False
+CORS(app)
+
+lemmatizer = WordNetLemmatizer()
+
+# Carga las palabras y las clases preprocesadas previamente
+words = pickle.load(open('words.pkl', 'rb'))
+classes = pickle.load(open('classes.pkl', 'rb'))
+
+model = load_model("chatbot_model.h5")
+
+vectorizer = CountVectorizer(lowercase=True, analyzer='word', tokenizer=nltk.word_tokenize, vocabulary=words)
+
+@app.route('/api', methods=['POST'])
+def handle_api_request():
+
+    data = request.get_json()
+    user_message = data.get('user_message')
+
+    print("User Message:", user_message) 
+
+    # Aqui mandaremos la response
+
+    # Genera la respuesta utilizando el modelo
+    response = generate_response(user_message)
+
+    print("Generated Response:", response)
+
+    response_data = {
+        'response' : response
+    }
+
+    return jsonify(response_data), 200, {'Content-Type': 'application/json; charset=utf-8'}
+
+def generate_response(user_message):
+
+    user_message_bag = preprocess_message(user_message, vectorizer)
+    predicted_probabilities = model.predict(np.array([user_message_bag]))
+    predicted_class = np.argmax(predicted_probabilities)
+    response = get_response_by_class(classes[predicted_class])
+    return response
+
+def preprocess_message(message, vectorizer):
+
+    words_list = nltk.word_tokenize(message)
+    print("Tokenized words:", words_list)  # Add this line to check tokenization
+    words_list = list(set([lemmatizer.lemmatize(word.lower()) for word in words_list]))
+
+    bag = [0] * len(vectorizer.get_feature_names_out())
+    for word in words_list:
+        if word in vectorizer.vocabulary_:
+            bag[vectorizer.vocabulary_[word]] = 1
+
+    return np.array(bag)
+
+def get_response_by_class(tag):
+
+    for intent in intents['intents']:
+        if intent['tag'] == tag:
+            return np.random.choice(intent['responses'])
+    
+    print("No se encontró una respuesta para la etiqueta.")
+    return "Lo siento, no entiendo lo que dices."
+
+if __name__ == '__main__':
+    app.run(debug=True)
---
+++ training.py
@@ -0,0 +1,73 @@
+import nltk
+nltk.download('punkt')
+nltk.download('wordnet')
+from nltk.stem import WordNetLemmatizer
+from sklearn.feature_extraction.text import CountVectorizer  # Add this line
+import json
+import pickle
+import numpy as np
+from tensorflow.keras import Sequential
+from tensorflow.keras.layers import Dense, Dropout
+from tensorflow.keras.optimizers import SGD
+
+lemmatizer = WordNetLemmatizer()
+words = []
+classes = []
+documents = []
+ignore_words = ['?', '!']
+data_file = open('intents.json').read()
+intents = json.loads(data_file)
+print(intents)
+
+for intent in intents['intents']:
+    for pattern in intent['patterns']:
+        w = nltk.word_tokenize(pattern)
+        words.extend(w)
+        documents.append((w, intent['tag']))
+        if intent['tag'] not in classes:
+            classes.append(intent['tag'])
+
+words = [lemmatizer.lemmatize(w.lower()) for w in words if w not in ignore_words]
+
+pickle.dump(words, open('words.pkl','wb'))
+pickle.dump(classes, open('classes.pkl','wb'))
+
+# Preparing a list of strings for the CountVectorizer
+pattern_strings = [' '.join(pattern) for pattern, _ in documents]
+
+# Creating the bag-of-words representation using CountVectorizer
+vectorizer = CountVectorizer(lowercase=True, analyzer='word', tokenizer=nltk.word_tokenize)
+X = vectorizer.fit_transform(pattern_strings).toarray()
+
+# Preparing training data
+max_words = len(vectorizer.vocabulary_)
+training_data = []
+for i, (_, intent) in enumerate(documents):
+    output_row = [0] * len(classes)
+    output_row[classes.index(intent)] = 1
+    training_data.append((X[i], output_row))
+
+# Separating training features (X) and labels (Y)
+train_x = np.array([x for x, _ in training_data])
+train_y = np.array([y for _, y in training_data])
+
+# Creating the model
+model = Sequential()
+model.add(Dense(128, input_shape=(max_words,), activation='relu'))
+model.add(Dropout(0.5))
+model.add(Dense(64, activation='relu'))
+model.add(Dropout(0.5))
+model.add(Dense(len(classes), activation='softmax'))
+
+# Using the legacy version of SGD optimizer
+sgd = SGD(lr=0.01, momentum=0.9, nesterov=True)
+model.compile(loss='categorical_crossentropy', optimizer=sgd, metrics=['accuracy'])
+
+# Saving the vectorizer's vocabulary
+pickle.dump(vectorizer.vocabulary_, open('vectorizer_vocab.pkl', 'wb'))
+
+# Fitting and saving the model
+hist = model.fit(train_x, train_y, epochs=300, batch_size=5, verbose=1)
+model.save('chatbot_model.h5', hist)
+
+print("Model created and trained.")
